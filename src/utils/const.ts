4b59d03433c5d132379bc8d651939392cdac2453
langhaidian <sergio13848@gmail.com>
2024-06-03 17:25:12 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -48,7 +48,7 @@ const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据 <br>"明明这么痛苦，这么难过，为什么就是不能放弃跑步? 因为全身细跑都在春置欲动，想要感受强风迎面吹拂的滋昧。"   <br>-《强风吹拂》`;
+  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据 \n"明明这么痛苦，这么难过，为什么就是不能放弃跑步? 因为全身细跑都在春置欲动，想要感受强风迎面吹拂的滋昧。"   <br>-《强风吹拂》`;
 };
 const ENGLISH_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
