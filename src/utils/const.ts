9a7e34b9c1b725e6866a0e493886f8a634cffa8c
langhaidian <sergio13848@gmail.com>
2024-06-03 17:00:58 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
   // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoibGFuZ2hhaWRpYW4iLCJhIjoiY2x3eXFsdDdtMXQybTJrb215bmJnOGRwdiJ9.DSHUsUkthvNRBDJMHGr0uw';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
@@ -48,7 +48,7 @@ const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据`;
+  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据 <br>"明明这么痛苦，这么难过，为什么就是不能放弃跑步? 因为全身细跑都在春置欲动，想要感受强风迎面吹拂的滋昧。"   <br>-《强风吹拂》`;
 };
 const ENGLISH_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
